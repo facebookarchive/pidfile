fec182c4af65aac0a4ece1aef69c234c2c2b81af
Naitik Shah <n@daaku.org>
2012-05-25 14:00:53 -0700

    initial commit

---
+++ pidfile.go
@@ -0,0 +1,24 @@
+// Package pidfile writes pid files.
+package pidfile
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+)
+
+var pidfile = flag.String("pidfile", "", "If specified, write a pid to file.")
+
+// Write the pidfile based on the flag, if one is set.
+func Write() {
+	if *pidfile == "" {
+		return
+	}
+	err := ioutil.WriteFile(
+		*pidfile, []byte(strconv.Itoa(os.Getpid())), os.FileMode(0644))
+	if err != nil {
+		log.Fatalf("Failed to write pidfile %s: %s", *pidfile, err)
+	}
+}
